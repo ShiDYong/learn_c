9ce5f694460bac28b1bcf82f91ea41f907d4d3b5
shiDyong <sdy1819@gmail.com>
2024-03-03 22:48:07 +0800

    练习题的优化

--- 03Formatted_Input_Output/chapter3_exercises.c
+++ 03Formatted_Input_Output/chapter3_exercises.c
@@ -6,7 +6,155 @@
 ********************************************************************************/
 #include<stdio.h>
 
-int main(void) {
-    printf("hello world\n");
-    return 0;
+/*
+ * Exercises 3.01
+ * What output do the following calls of 'printf' product?
+ */
+void exec_test01() {
+    printf("%6d, %4d\n", 86, 1040);
+    printf("%12.5e\n", 30.253);
+    printf("%.4f\n", 83.162);
+    printf("%-6.2g\n", .0000009979);
+    /*%：格式化字符串的起始标记。
+        -：表示左对齐输出，即输出的字符串左对齐，右边用空格填充。
+        6：表示输出的最小字符宽度为6，即至少输出6个字符宽度，如果输出的字符不足6个，则在左边用空格填充。
+        .2：表示精度为2，即小数点后保留2位有效数字。
+        g：表示浮点数格式，根据值的大小自动选择使用%e或%f的输出格式，以较短的形式输出。
+        接下来，我们将0.0000009979按照给定的格式化字符串进行输出：
+        小数点后保留2位有效数字，而0.0000009979有7位有效数字，因此它会被四舍五入为0.000001。
+        输出的宽度为6，但小数点和数字共占据了7个字符的位置，所以输出的结果是7个字符，即"0.000001"。
+        因为使用了左对齐（-），所以在输出的数字后面会用空格填充到6个字符的宽度。*/
+    //输出：
+    /*    86, 1040
+ 3.02530e+01
+83.1620
+1e-06 */
 }
+
+/*
+ * Exercise 3.02
+    Write calls of `printf` that display a `float` variable `x` in the following
+    formats.
+    (a) Exponential notation; left-justified in a field of size 8: one digit after
+    the decimal point.
+    (b) Exponential notation: right-justified in a field of size 10; six digits
+    after the decimal point.
+    (c) Fixed decimal notation; left-justified in a field of size 8; three digits
+    after the decimal point.
+    (d) Fixed decimal notation; right-justified in a field of size 6; no digits
+    after the decimal point.
+ */
+void exec_test02() {
+    float x;
+    printf("%-8.1e\n", x);
+    printf("%10.6e\n", x);
+    printf("%-8.3f\n", x);
+    printf("%6.0f\n", x);
+
+}
+
+/*
+ * Exercise 3.03
+    For each of the following pairs of `scanf` format strings, indicate whether
+    the two strings are equivalent. If they're not, show how they can be
+    distinguished.
+    scanf函数读取数据时，只忽略数据前的空白字符，其它无效的输入字符都会导致读取失败。因此
+    一般除了转换说明，格式串一般不包含其他形式的字符，例如逗号、冒号等。如果添加来这些字符，用户
+    在输入数据时必须严格按照scanf函数中的格式串形式输入.
+ */
+void exec_test03() {
+    int i;
+    float f;
+    printf("Enter a number: ");
+    //scanf("%d", &i);//虽然缺少空格符，但是函数会忽略数据之前的空白，因此最终结果和第二个格式串一样的
+    //scanf(" %d", &i);
+    //scanf(" % d", &i); 这种输入方式是无效的
+    // scanf("%d ", &i);
+    /*在这种情况下，格式串以%d结束，并且在%d之后有一个空格字符。这意味着scanf会尝试读取一个整数，
+     * 并且在读取整数后期望找到一个额外的空格字符。如果用户在输入整数后输入了空格字符，
+     则scanf将会略过它们。但是，如果用户输入数字后直接按下回车键，而不添加额外的空格字符，
+     scanf将会等待用户输入更多字符，直到用户输入一个非空格字符或按下Enter键。
+     因此，这种方式可能导致程序在用户没有预期的情况下暂停等待输入。
+     * */
+    scanf("%d-%d-%d\n", &i, &i, &i);
+    scanf("%d -%d -%d\n", &i, &i, &i); //推荐使用
+    //假设输入以下数据：
+    /*
+     * a: 45-56-67
+     * b: 45.-56.-67
+     * c: 45-.56-.67
+     * d: 45.-.56.-.67
+     * 上面四种输入只有第二种格式化都成功读取到，第一种格式化只有a这种能成功读取到。
+     * */
+    scanf("%f", &f);
+    scanf("%f ", &f);//两者时等价的
+    scanf("%f,%f", &f, &f);
+    scanf("%f, %f", &f, &f);//两者是等价的
+    printf("%d\n", i);
+}
+
+/*
+ * Exercise 3.04
+Suppose that we call `scanf` as follows:
+ */
+void exec_test04() {
+    int x, z;
+    float y;
+    printf("Enter three numbers: ");
+    scanf("%d%f%d", &x, &y, &z);
+    //如果输入：10.3 5 6
+    //那么输出是多少？:10 0.300000 5
+    printf("%d %f %d\n", x, y, z);
+    /*
+     * scanf函数在处理格式串和用户实际输入字符时的基本方式是依次读取并与格式串中的字符和转换进行对比
+     * 如果匹配则进行转换，否则会把不同的数据返回原处，并留给下一次scanf函数调用：
+     * 数据输入的完整过程是这样的：用户输入的10可以完整匹配第一个%d转换说明，当读取到小数点时，
+     * scanf 将小数点和第一个% d 进行匹配，确认第一个整型数据输入完成，
+     * 即x的值为10；然后 scanf 会将小数点和第二个转换说明% f 进行匹配，
+     * 匹配成功后继续读取，直到遇到下一个空格，因此将．3作为变量 x 的值。
+     * 输入数据5之前的空白被忽略，因此第三个转换说明将5作为变量 j 的值，
+     * 最后剩余的字符会被 scanf 放回原处，等待下一次scanf 函数调用。
+     * 参考答案最终变量 i 的值为10；变量 x 的值为．3，即0.3；变量 j 的值为5
+     * */
+
+}
+/*
+ *  Exercise 3.05
+Suppose that we call `scanf` as follows:
+ scanf("%f%d%f", &x, &i, &y);
+ If the user enters
+ 12.3 45.6 789
+what will be the values of `x`, `i` and `y` after the call? (Assume that `x` and
+`y` are `float` variables and `i` is an `int` variable.)
+ */
+void exec_test05(){
+    //同上一题目
+    //输出：
+    // x = 12.3
+    //i = 45
+    //y = 0.6
+}
+
+//int main(void) {
+//    int i, j;
+//    printf("Enter two numbers: "); //输入4,28
+//    scanf("%d%d", &i, &j);
+//    printf("%d%d\n",i,j); //输出结果却是4,1
+    /*分析原因：
+     * 为什么在输入4和28后，输出的结果是 "4 1" 呢？
+     * 这是因为代码中并没有检查 scanf 函数的返回值。scanf 函数返回成功读取并赋值的参数个数。
+     * 在这种情况下，它返回值为1，因为只有第一个输入成功了（也就是4被成功地存储在了变量 i 中），
+     * 而第二个输入由于缺少一个逗号而导致失败，因此没有被赋值给 j。
+     * 而在C语言中，未初始化的变量会被默认赋值为0，因此变量 j 的值是0，而不是28
+     * */
+    //针对上面问题的解决方式
+//    printf("Enter two numbers: ");
+//    scanf("%d,%d", &i, &j);//数与数之间加上逗号来分割
+//    printf("%d %d\n", i, j);
+    // exec_test01();
+    //exec_test02();
+    //exec_test03();
+//    exec_test04();
+//
+//    return 0;
+//}
--- 03Formatted_Input_Output/chapter3_programming.c
+++ 03Formatted_Input_Output/chapter3_programming.c
@@ -4,6 +4,8 @@ Created by mason on 2023/4/31.
 */
 
 #include <stdio.h>
+#include <stdbool.h>
+#include<ctype.h>
 
 /**
  * 练习1:编写一个程序，以月/日/年(mm/dd/yy)的格式接受用户输入的日志信息，并以年月日(yyyymmdd)的格式展示出来
@@ -13,18 +15,91 @@ void exec3_1() {
     printf("Please enter a date(mm/dd/yyyy): ");
     //获取用户的输入，如何处理格式是mm/dd/yy
     // scanf("%d%d%d", &month, &day, &year);获取不到/后面的数据了
-    scanf("%d/%d/%d", &month, &day, &year);//和下面的效果是一样的，因为scanf函数自动去除空白字符
-    //scanf("%d /%d /%d", &month, &day, &year);
+    // scanf("%d/%d/%d", &month, &day, &year);//和下面的效果是一样的，因为scanf函数自动去除空白字符
+    scanf("%d /%d /%d", &month, &day, &year); //添加空格是为来提高用户输入的容错性
     //这里如何让输出格式是：20221231呢，最开始方式是有问题的
-    printf("Your enter the date is:%d%d%d\n", year, month, day);
+    printf("Your enter the date is:%4d%02d%02d\n", year, month, day);
     //第二种写法更准确：以十进制形式显示变量i，并至少有4位数字符合年的写法，同理月、日写法也是一样的
     printf("Your enter the date is: %0.4d%0.2d%0.2d\n", year, month, day);
 
 }
+/*
+ * 检查这年是平年还是闰年
+ */
+bool isLeapYear(int year) {
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
+
+/*
+ * 检查输入的日期是否合法，计算是闰年还是平年
+ */
+bool isValidDate(int month, int day, int year) {
+    //检查月份
+    if (month < 1 || month > 12) return false;
+    //每年中除了二月份天数不确定，其它时间都是确定的
+    int daysInMonth[] = {31, 28 + isLeapYear(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    //检查输入的天数
+    if (day < 1 || day > daysInMonth[month - 1]) return false;
+    return true;
+
+
+}
+
+//Function to convert to date to yyyymmdd formant
+void convertToDate(int month, int day, int year) {
+    printf("You enter the date %4d%02d%02d\n", year, month, day);
+}
 
 /**
- * 练习2:编写一个程序，对用户输入的产品信息进行格式化，其中产品编号和日期项采用左对齐方式，单位价格
- * 采用右对齐方式，允许美元金额最大取值9999.99。提示：各个列使用制表符控制
+ * 对上面的题目进一步优化，提高程序的可靠性和安全性
+ */
+bool exec3_1_optimization() {
+    int year, month, day;
+    char slash;
+    printf("Enter a date (mm/dd/yyyy): ");
+    //Read input date
+    /*scanf("%d %d %d", &month, &day, &year) != 3：
+      这一部分使用scanf函数尝试从标准输入中读取三个整数，即月份、日期和年份。如果scanf函数成功读取了三个整数，
+      则表达式的结果为假（0），否则为真（非0）。因此，如果用户输入的是有效的日期格式，即正确地输入了三个整数（月、日、年），
+      这部分条件将为假，代码不会执行下面的语句。
+      || scanf("%c", &slash) != 0：如果上述scanf函数读取失败，即用户没有输入三个整数，则会尝试再次使用scanf函数来读取一个字符。
+      这个字符应该是日期中的斜杠 /。如果读取成功，则表达式的结果为真，否则为假。如果用户输入的是非数字字符，
+      或者缺少斜杠 /，这部分条件将为真，代码会执行下面的语句。
+      || slash != '/'：这一部分检查在第二步中读取的字符是否为斜杠 /。如果不是斜杠 /，则表达式的结果为真，否则为假。如果输入的字符不是斜杠 /，则表示日期格式不正确，代码会执行下面的语句。
+|| getchar() != '\n'：
+       最后一部分检查输入流中是否还有其他字符。getchar()函数尝试从输入流中读取一个字符，并将其与换行符 \n 进行比较。
+       如果输入流中还有其他字符（换行符之后），或者用户在输入完日期后没有按下 Enter 键，那么这部分条件将为真，代码会执行下面的语句。
+*/
+    //|| scanf("%c", &slash) != 0 || slash != '/' || getchar() != '\n'
+    if (scanf("%d /%d /%d", &month, &day, &year) != 3) {
+        printf("Invalid input format.\n");
+        return false;
+    }
+    //Validate input date
+    if (!isValidDate(month, day, year)) {
+        printf("Invalid date.\n");
+        return false;
+    }
+    //Convert and print the date
+    convertToDate(month, day, year);
+    return true;
+
+}
+
+/**
+ * Project 3.02
+  Write a program that formats product information entered by the user. A session
+  with the program should look like this:
+    Enter item number: 583
+    Enter unit price: 13.5
+    Enter purchase date (mm/dd/yyyy): 10/24/2010
+    Item            Unit            Purchase
+                    Price           Date
+    583             $  13.50        10/24/2010
+    The item number and date should be left justified; the unit price should be
+    right justified. Allow dollar amounts up to $9999.99. *Hint* Use tabs to line up
+    the columns.
  */
 void exec3_2() {
     int item, year, month, day;
@@ -34,17 +109,39 @@ void exec3_2() {
     printf("Enter unit price: ");
     scanf("%f", &unit_price);
     printf("Enter purchase date(mm/dd/yyyy): ");
-    scanf("%d/%d/%d", &month, &day, &year);
+    scanf("%d /%d /%d", &month, &day, &year);
     //如何输出来？思路应该是要打印成2行数据，第一行展示字段，第二行展示字段内容，第一次处理方式是错误的
     //printf("Item\tUnit\nPrice\tPurchase\nDate\t",item,unit_price,month,day,year);
-    printf("Item\t\tUnit\t\tPurchase\n\t\tPrice\t\tDate\n");
-    printf("%d\t\t$%8.2f\t%.2d/%.2d/%.4d\n", item, unit_price, month, day, year);
+    // Displaying formatted product information
+    printf("Item\t\tUnit\t\tPurchase\n");
+    printf("\t\tPrice\t\tDate\n");
+    printf("%-d\t\t$%7.2f\t%-02d/-%02d/-%4d\n", item, unit_price, month, day, year);
 
 
 }
 
 /**
- * 练习3:输入一个国际图书标准编号(ISBN)进行识别并输出来
+ *  Project 3.03
+    Books are identified by an International Standard Book Number (ISBN). ISBNs
+    assigned after January 1, 2007 contain 13 digits, arranged in five groups, such
+    as 978-0-393-97950-3. (Older ISBNs use 10 digits.) The first group (the *GSI
+    prefix*) is currently either 978 or 979. The *group identifier* specifies the
+    language or country of origin (for example, 0 and 1 are used in English-speaking
+    countries). The *publisher code* identifies the publisher (393 is the code for
+    W. W. Norton). The *item number* is assigned by the publisher to identify a
+    specific book (97950 is the code for this book). An ISBN ends with a *check
+    digit* that's used to verify the accuracy of the preceding digits. Write a
+    program that breas down an ISBN entered by the user:
+        Enter ISBN: 978-0-393-97950-3
+        GSI prefix: 978
+        Group identifier: 0
+        Publisher code: 393
+        Item number: 97950
+        Check digit: 3
+    *Note*: The number of digits in each group may very: you can't assume that
+    groups have the lengths shown in this example. Test your program with actual
+    ISBN values (usually found on the back cover of a book and on the copyright
+    page).
  */
 void exec3_3() {
     int GSI_prefix, group_identifier, publisher_code, item_number, check_digit;
@@ -64,14 +161,14 @@ void exec3_4() {
     int first, second, last;
     printf("Enter phone number [(xxx) xxx-xxxx]: ");
     scanf("(%d) %d-%d", &first, &second, &last);
-    printf("Your enter is:%0.3d.%0.3d.%0.4d\n",first,second,last);
+    printf("Your enter is:%0.3d.%0.3d.%0.4d\n", first, second, last);
 }
 
 /**
  * 编程练习5:编写一个程序，要求用户输入(按照任意次序)输入1～16的所有整数，然后用4*4的矩阵的形式将它们显示出来
  * ，再计算出每行、每列和每条对角线上的和
  */
-void exec3_5(){
+void exec3_5() {
     //此题目后面再写
 
 }
@@ -79,21 +176,22 @@ void exec3_5(){
 /**
  * 编程练习6:修改3.2节的addfrac.c程序，使使用可以同时输入两个分数，中间用加号隔开；
  */
-void exec3_6(){
-    int num1,demon1,num2,demon2,result_num,result_demon;
+void exec3_6() {
+    int num1, demon1, num2, demon2, result_num, result_demon;
     printf("Enter two fractions separated by a plus sign: ");
     //这里加不加+都是可以的
-    scanf("%d/%d+%d/%d",&num1,&demon1,&num2,&demon2);
-    result_num = num1 * demon2+ num2*demon1;
+    scanf("%d/%d+%d/%d", &num1, &demon1, &num2, &demon2);
+    result_num = num1 * demon2 + num2 * demon1;
     result_demon = demon1 * demon2;
-    printf("The sum is: %d/%d",result_num,result_demon);
+    printf("The sum is: %d/%d", result_num, result_demon);
 }
 
-//int main() {
-//    //exec3_1();
-//    //exec3_2();
-//   // exec3_3();
-//  // exec3_4();
-//  exec3_6();
-//    return 0;
-//}
+int main() {
+    //exec3_1();
+    //exec3_2();
+     exec3_3();
+    // exec3_4();
+    //exec3_6();
+    //exec3_1_optimization();
+    return 0;
+}
